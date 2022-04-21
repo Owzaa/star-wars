6466a7b65521dbbb9c4656a53538f209d1cbf0e5
Owzaa <nyondo.olwethu@gmail.comm>
2022-04-21 21:37:01 +0200

    commit on master

--- src/components/ListePeople/ListFilms.js
+++ src/components/ListePeople/ListFilms.js
@@ -9,31 +9,30 @@ export const List = ({ films }) => {
 
   const { DataisLoaded, items } = this.state;
   if (!DataisLoaded) return <>
-  <h4> Please kindly wait while we fetch your Film List [Data] .... </h4> </> ;
-  return(
-  <div>
-    <h1>LISTED FILMS</h1> 
- 
-    <Grid  sx={{ flexGrow: 1,overflow:'hidden',px:1 }} spacing={3} >  
-  {films.map((film,id) => ( 
-    <Card row sx={{ maxWidth: 450 }}>
-   <CardActionArea>
-  
-  <ul> 
-    < CardContent>
-            
-      <Typography variant="inherit" component="paragraph">      
-        <p key={id=film.id} id="TitleText"><strong>{film.name}</strong> </p>
-        <p id="TitleTextContent">{film.title}</p> 
-        <p id="TextContent">{film.release_date}</p> 
-      </Typography>   
-     </CardContent>
-  </ul>  
-  </CardActionArea>
-  </Card>    
-  ) )}
-    
-  </Grid>    
-   </div> 
+    <h4> Please kindly wait while we fetch your Film List [Data] .... </h4> </>;
+  return (
+    <div>
+      <h1>LISTED FILMS</h1>
+      <Grid sx={{ flexGrow: 1, overflow: 'hidden', px: 1 }} spacing={3} >
+        {films.map((film, id) => (
+          <Card row sx={{ maxWidth: 450 }}>
+            <CardActionArea>
+
+              <ul>
+                < CardContent>
+
+                  <Typography variant="inherit" component="body">
+                    <p key={id = film.id} id="TitleText"><strong>{film.name}</strong> </p>
+                    <p id="TitleTextContent">{film.title}</p>
+                    <p id="TextContent">{film.release_date}</p>
+                  </Typography>
+                </CardContent>
+              </ul>
+            </CardActionArea>
+          </Card>
+        ))}
+
+      </Grid>
+    </div>
   )
 }
